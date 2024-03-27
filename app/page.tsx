a6814d6dd804e219a6641fdc5aec4f77a1236d42
gitFILO <x2xgudwls@g.skku.edu>
2024-03-27 18:36:15 +0900

    new commited - page push test

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,8 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel. 
+            this is new pushed!
           </p>
           <Link
             href="/login"
